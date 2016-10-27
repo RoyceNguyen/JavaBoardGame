2f47b396e142a6cc18d603235bb64e7f68d1d0e1
Jared Antle <Jared.Antle15@stclairconnect.ca>
2016-10-27 17:02:54 -0400

    Added title image, centered image and bottom buttons.

--- src/ClubForm.java
+++ src/ClubForm.java
@@ -1,7 +1,10 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -13,10 +16,20 @@ public class ClubForm extends Application{
 	@Override
 	public void start(Stage stage) throws Exception {
 		BorderPane border = new BorderPane();
+		
+		Image img = new Image("boardgame.png");
+		ImageView imgVw = new ImageView();
+		imgVw.setImage(img);
+		imgVw.setFitWidth(300);
+		imgVw.setFitHeight(100);
+		
 		HBox top = new HBox();
-		top.setPadding(new Insets(60,60,60,60));
+		top.setPadding(new Insets(10,10,10,10));
 		top.setSpacing(10);
 		top.setStyle("-fx-background-color: #336699;");
+		top.getChildren().add(imgVw);
+		top.setAlignment(Pos.CENTER);
+		
 		HBox bottom = new HBox();
 		bottom.setPadding(new Insets(10,10,10,10));
 		bottom.setSpacing(10);
@@ -27,6 +40,7 @@ public class ClubForm extends Application{
 		Button clear = new Button("Clear");
 		clear.setPrefSize(100, 20);
 		bottom.getChildren().addAll(submit, clear);
+		bottom.setAlignment(Pos.CENTER);
 		
 		GridPane grid = new GridPane();
 		grid.setStyle("-fx-background-color: #FFFF00;");
@@ -34,6 +48,7 @@ public class ClubForm extends Application{
 		border.setCenter(grid);
 		border.setBottom(bottom);
 		Scene scene = new Scene(border, 500, 700);
+		stage.setTitle("Board Game Club");
 		stage.setScene(scene);
 		stage.show();
 	}
