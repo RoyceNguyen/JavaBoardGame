83a6814cb199208a97c7bd57e685a9c8c6e7daac
Blaze Blackbird <blaze.blackbird01@stclairconnect.ca>
2016-10-27 17:25:11 -0400

    Finished the tasks of creating radio buttons and textfields for form

--- src/ClubForm.java
+++ src/ClubForm.java
@@ -2,8 +2,10 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -23,8 +25,8 @@ public class ClubForm extends Application {
 		grid.setPadding(new Insets(10, 10, 10, 10));
 		grid.setVgap(5);
 		grid.setHgap(5);
-		//Create the firstName text field
 		
+		//Create the firstName text field
 		final TextField name = new TextField();
 		Label fnameLabel = new Label("First Name:");
 		GridPane.setConstraints(fnameLabel, 0, 0);
@@ -43,8 +45,8 @@ public class ClubForm extends Application {
 		lastName.setPromptText("Enter your last name.");
 		GridPane.setConstraints(lastName, 1, 1);
 		grid.getChildren().add(lastName);
-		//Create the email text field
 		
+		//Create the email text field
 		final TextField email = new TextField();
 		Label emailLabel = new Label("Email:");
 		GridPane.setConstraints(emailLabel, 0, 2);
@@ -53,8 +55,8 @@ public class ClubForm extends Application {
 		email.setPromptText("Enter your email.");
 		GridPane.setConstraints(email, 1, 2);
 		grid.getChildren().add(email);
-		//Create the phone text field
 		
+		//Create the phone text field
 		final TextField phone = new TextField();
 		Label phoneLabel = new Label("Phone:");
 		GridPane.setConstraints(phoneLabel, 0, 3);
@@ -64,12 +66,31 @@ public class ClubForm extends Application {
 		GridPane.setConstraints(phone, 1, 3);
 		grid.getChildren().add(phone);
 		
+		//Create radio buttons for gender
+		final ToggleGroup group = new ToggleGroup();
+		Label gender = new Label("Select Gender");
+		GridPane.setConstraints(gender, 2, 0);
+		gender.setPadding(new Insets(0, 0, 0, 50));
+		grid.getChildren().add(gender);
+		RadioButton radio1 = new RadioButton("Male");
+		RadioButton radio2 = new RadioButton("Female");
+		radio1.setToggleGroup(group);
+		radio2.setToggleGroup(group);
 		
-		/*Scene scene = new Scene(grid,500, 500);
+		radio1.setPadding(new Insets(0, 0, 0, 50));
+		radio2.setPadding(new Insets(0, 0, 0, 50));
+		grid.getChildren().addAll(radio1, radio2);
+		GridPane.setConstraints(radio1, 2, 1);
+		GridPane.setConstraints(radio2, 2, 2);
+		
+		
+		
+		
+		Scene scene = new Scene(grid);
 		primaryStage.setTitle("BOARDGAME");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		*/
+		
 		
 		
 		
