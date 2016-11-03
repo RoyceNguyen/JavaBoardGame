c588c9ffc56efe6b6ea4900b5b803c7cac5b6f57
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2016-11-03 15:40:01 -0400

    Issues walkaround

--- src/ClubForm.java
+++ src/ClubForm.java
@@ -12,8 +12,11 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
@@ -40,6 +43,68 @@ public class ClubForm extends Application{
 		//Create border pane for the whole form
 		BorderPane border = new BorderPane();
 		
+		//Creating a GridPane container
+				GridPane grid = new GridPane();
+				grid.setPadding(new Insets(10, 10, 10, 10));
+				grid.setVgap(5);
+				grid.setHgap(5);
+				
+				//Create the firstName text field
+				final TextField name = new TextField();
+				Label fnameLabel = new Label("First Name:");
+				GridPane.setConstraints(fnameLabel, 0, 0);
+				grid.getChildren().add(fnameLabel);
+				name.setPromptText("Enter your first name.");
+				name.setPrefColumnCount(10);
+				name.getText();
+				GridPane.setConstraints(name, 1, 0);
+				grid.getChildren().add(name);
+				
+				//Create the lastName text field
+				final TextField lastName = new TextField();
+				Label lnameLabel = new Label("Last Name:");
+				GridPane.setConstraints(lnameLabel, 0, 1);
+				grid.getChildren().add(lnameLabel);
+				lastName.setPromptText("Enter your last name.");
+				GridPane.setConstraints(lastName, 1, 1);
+				grid.getChildren().add(lastName);
+				
+				//Create the email text field
+				final TextField email = new TextField();
+				Label emailLabel = new Label("Email:");
+				GridPane.setConstraints(emailLabel, 0, 2);
+				grid.getChildren().add(emailLabel);
+				email.setPrefColumnCount(15);
+				email.setPromptText("Enter your email.");
+				GridPane.setConstraints(email, 1, 2);
+				grid.getChildren().add(email);
+				
+				//Create the phone text field
+				final TextField phone = new TextField();
+				Label phoneLabel = new Label("Phone:");
+				GridPane.setConstraints(phoneLabel, 0, 3);
+				grid.getChildren().add(phoneLabel);
+				phone.setPrefColumnCount(10);
+				phone.setPromptText("Enter your phone number.");
+				GridPane.setConstraints(phone, 1, 3);
+				grid.getChildren().add(phone);
+				
+				//Create radio buttons for gender
+				final ToggleGroup group = new ToggleGroup();
+				Label gender = new Label("Select Gender");
+				GridPane.setConstraints(gender, 2, 0);
+				gender.setPadding(new Insets(0, 0, 0, 50));
+				grid.getChildren().add(gender);
+				RadioButton radio1 = new RadioButton("Male");
+				RadioButton radio2 = new RadioButton("Female");
+				radio1.setToggleGroup(group);
+				radio2.setToggleGroup(group);
+				
+				radio1.setPadding(new Insets(0, 0, 0, 50));
+				radio2.setPadding(new Insets(0, 0, 0, 50));
+				grid.getChildren().addAll(radio1, radio2);
+				GridPane.setConstraints(radio1, 2, 1);
+				GridPane.setConstraints(radio2, 2, 2);
 		//add title image and put it in an image view
 		Image img = new Image("boardgame.png");
 		ImageView imgVw = new ImageView();
@@ -92,7 +157,7 @@ public class ClubForm extends Application{
 		genre.setPrefHeight(100);
 		genre.setItems(genres);
 		VBox intro = new VBox();
-		intro.getChildren().addAll(tellus,introduction,gameGenre,genre);
+		intro.getChildren().addAll(grid,tellus,introduction,gameGenre,genre);
 		
 
 		Button submit = new Button("Submit");
@@ -103,6 +168,18 @@ public class ClubForm extends Application{
 			public void handle(Event event) {
 				// TODO Auto-generated method stub
 				try (BufferedWriter bw = new BufferedWriter(new FileWriter("info.txt", true))) {
+					bw.write(name.getText());
+					bw.newLine();
+					bw.write(lastName.getText());
+					bw.newLine();
+					bw.write(email.getText());
+					bw.newLine();
+					bw.write(phone.getText());
+					bw.newLine();
+					bw.write(radio1.getText());
+					bw.newLine();
+					bw.write(radio2.getText());
+					bw.newLine();
 			        bw.write(introduction.getText());
 			        bw.newLine();
 			        bw.write(genre.getSelectionModel().getSelectedItem());
@@ -121,6 +198,12 @@ public class ClubForm extends Application{
 			@Override
 			public void handle(Event event) {
 				// TODO Auto-generated method stub
+				name.clear();
+				lastName.clear();
+				email.clear();
+				phone.clear();
+				radio1.setSelected(false);
+				radio2.setSelected(false);
 				introduction.clear();
 				genre.getSelectionModel().clearSelection();
 			}
@@ -142,7 +225,7 @@ public class ClubForm extends Application{
 		border.setCenter(intro);
 		border.setBottom(bottom);
 		//set the scene with the border pane
-		Scene scene = new Scene(border, 500, 700);
+		Scene scene = new Scene(border, 500, 530);
 		stage.setTitle("Board Game Club");
 		stage.setScene(scene);
 		stage.show();
